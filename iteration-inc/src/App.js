dc2cec89a20474fbbe1eb8ea530acf7f6955c008
HamishMAD <hamishmdawson@gmail.com>
2025-01-22 14:47:43 +0000

    Basic react setup and ready for production

---
+++ iteration-inc/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hi guys Hamish wrote this line here look how cool this is Edit</p>
+        <p>This line can be edited in app.js!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
