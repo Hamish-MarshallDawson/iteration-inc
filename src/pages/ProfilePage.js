766bca6f6c281c12e3ae834f1330371913b827ae
Billy-Jack <bg2018.hw.ac.uk>
2025-02-15 18:24:32 +0000

    Made ProfilePage file; testing commit

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,15 @@
+// Filename - pages/about.js
+
+import React from "react";
+
+import "../App.css";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>Check your profile here</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
