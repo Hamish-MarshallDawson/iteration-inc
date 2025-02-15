c30b308f3b520779dc56aebc6cc9c000792a398f
HamishMAD <hamishmdawson@gmail.com>
2025-02-15 21:27:42 +0000

    test
    testing

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,74 +1,67 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../css/login-page.css";
 
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from "react-router-dom"
-import '../css/login-page.css';
-
-
+// testing something
 function Login() {
+  //these are state variables
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    //these are state variables
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-
-    const handleLogin = (e) => {
-        e.preventDefault();
-
-        if (email === "123@bombaclat.com" && password === "123456") {
-            alert("Login successful");
-            navigate('/authentication');
-        } else {
-            alert("Login failed");
-        }
-        
+    if (email === "123@bombaclat.com" && password === "123456") {
+      alert("Login successful");
+      navigate("/authentication");
+    } else {
+      alert("Login failed");
     }
+  };
 
+  return (
+    <div className="login">
+      <h1>Login</h1>
 
-    return (
-        <div className="login">
-            <h1>Login</h1>
-
-            <form onSubmit={handleLogin}>
-                <div className="input">
-                    <label>Email:</label>
-                    <input 
-                        type="email" 
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required 
-                    />
-                </div>
-                
-                <div className="input">
-                    <label>Password:</label>
-                    <input 
-                        type="password" 
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required   
-                    />
-                </div>
-
-                <button type="submit" className="submit-button">
-                    Login
-                </button>
-             
-            </form>
-
-            <div className="helper-text">
-                <p>
-                    <Link to="/passwordReset">Forgot Password?</Link>
-                </p>
-                <p>
-                     Don't have an account? <Link to="/sign-up">Sign Up</Link>
-                </p>
-            </div>
-         
+      <form onSubmit={handleLogin}>
+        <div className="input">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
-    );
+
+        <div className="input">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit" className="submit-button">
+          Login
+        </button>
+      </form>
+
+      <div className="helper-text">
+        <p>
+          <Link to="/passwordReset">Forgot Password?</Link>
+        </p>
+        <p>
+          Don't have an account? <Link to="/sign-up">Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
