7b674e0b100eaa1c0fabb4561bcdb16c54210164
HamishMAD <hamishmdawson@gmail.com>
2025-03-06 14:31:33 +0000

    Massive refactoring of app.js (moved basically all of it into routes.js) and all content should work on zoomed screen now

--- src/App.js
+++ src/App.js
@@ -1,66 +1,13 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+// import Navbar from "./components/nav.js";
 
-import Navbar from "./components/nav.js";
-
-import Login from "./pages/Login.js";
-import Bedroom from "./pages/Bedroom.js";
-import Kitchen from "./pages/Kitchen.js";
-import LivingRoom from "./pages/LivingRoom.js";
-import EnergyReport from "./pages/EnergyReport.js";
-import Awards from "./pages/Awards.js";
-import ProfilePage from "./pages/ProfilePage.js";
-
-import PasswordReset from "./pages/PasswordReset.js";
-import PasswordReset2 from "./pages/PasswordReset2.js";
-import SignUp from "./pages/SignUp.js";
-import Verify from "./components/Verify"; 
-import SignUp2 from "./pages/SignUp2.js";
-
-
+import RouteComponent from "./components/route.js";
 
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <MainContent />{" "}
-        {/* Move everything inside a separate component that has access to useLocation */}
-      </div>
-    </Router>
-  );
-}
-
-// Separate component for conditional rendering
-function MainContent() {
-  const location = useLocation(); // Get current route
-
-  return (
     <>
-      {/* Render Navbar only if NOT on login page */}
-      {location.pathname !== "/" && <Navbar />}
-
-      <Routes>
-        <Route path="/" element={<Login />} />
-        {/* <Route path="/login" element={<Login />} />{" "} */}
-        {/* This is the route to the Login page in directory ./pages/Login.js, imported in the header */}
-        <Route path="/Bedroom" element={<Bedroom />} />
-        <Route path="/Kitchen" element={<Kitchen />} />
-        <Route path="/Livingroom" element={<LivingRoom />} />
-        <Route path="/Energyreport" element={<EnergyReport />} />
-        <Route path="/Awards" element={<Awards />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/passwordReset" element={<PasswordReset />} />
-        <Route path="/passwordReset2" element={<PasswordReset2 />} />
-        <Route path="/verify" element={<Verify />} />
-        <Route path="/sign-up-2" element={<SignUp2 />} />
-      </Routes>
+      <RouteComponent />
     </>
   );
 }
---
+++ src/components/route.js
@@ -0,0 +1,72 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+import Login from "../pages/Login.js";
+import Bedroom from "../pages/Bedroom.js";
+import Kitchen from "../pages/Kitchen.js";
+import LivingRoom from "../pages/LivingRoom.js";
+import EnergyReport from "../pages/EnergyReport.js";
+import Awards from "../pages/Awards.js";
+import ProfilePage from "../pages/ProfilePage.js";
+
+import PasswordReset from "../pages/PasswordReset.js";
+import PasswordReset2 from "../pages/PasswordReset2.js";
+import SignUp from "../pages/SignUp.js";
+import Verify from "./Verify.js";
+import SignUp2 from "../pages/SignUp2.js";
+
+import Navbar from "./nav.js"; // Ensure correct import
+
+function MainContent() {
+  const location = useLocation(); // Get current route
+
+  // Define routes where the Navbar should be hidden
+  const hideNavbarRoutes = [
+    "/",
+    "/sign-up",
+    "/passwordReset",
+    "/passwordReset2",
+  ];
+
+  return (
+    <>
+      <div className="structure">
+        {/* Render Navbar only if NOT in the hideNavbarRoutes list */}
+        {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+
+        <div className="MainContent">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/Bedroom" element={<Bedroom />} />
+            <Route path="/Kitchen" element={<Kitchen />} />
+            <Route path="/Livingroom" element={<LivingRoom />} />
+            <Route path="/Energyreport" element={<EnergyReport />} />
+            <Route path="/Awards" element={<Awards />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/passwordReset" element={<PasswordReset />} />
+            <Route path="/passwordReset2" element={<PasswordReset2 />} />
+            <Route path="/verify" element={<Verify />} />
+            <Route path="/sign-up-2" element={<SignUp2 />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function RouteComponent() {
+  return (
+    <Router>
+      <div className="App">
+        <MainContent />
+      </div>
+    </Router>
+  );
+}
+
+export default RouteComponent;
