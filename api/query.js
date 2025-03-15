d62822840c33c7856abb4f07486514bacf9fdeb9
Billy-Jack <bg2018.hw.ac.uk>
2025-03-15 17:01:13 +0000

    attempting to debug

--- api/query.js
+++ api/query.js
@@ -20,6 +20,8 @@ export default async function handler(req, res) {
       return acc;
     }, {});
 
+    console.log(energy)
+
     return res.status(200).json({ groupedData });
     
   } catch (error) {
